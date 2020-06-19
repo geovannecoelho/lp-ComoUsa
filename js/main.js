b39abbe84d7e85606b7007e797bdb86e198b851a
new name <new email>
2020-06-19 13:52:21 -0300

    Upload Files

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+// Animação Menu
+const menuItems = document.querySelectorAll('.menu-nav a[href^="#"]');
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+	window.scroll({
+	top: to,
+	behavior: "smooth",
+	})
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
+
+
+
+// Animação itens
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+  
+  const target = document.querySelectorAll('[data-anime]');
+  const animationClass = 'animate';
+  
+  function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+      if((windowTop) > element.offsetTop) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    })
+  }
+  
+  animeScroll();
+  
+  if(target.length) {
+    window.addEventListener('scroll', debounce(function() {
+      animeScroll();
+    }, 200));
+  }
+
+  // Animação Botão topo
+  var btn = $('#button');
+
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+    });
+
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    });
